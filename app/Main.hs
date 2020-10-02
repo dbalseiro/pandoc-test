16a5214fe0eaa72f5bca0078d46df6db27778109
Diego Balseiro <dbalseiro@stackbuilders.com>
2020-10-02 11:29:24 -0500

    Test pandoc oldie

--- app/Main.hs
+++ app/Main.hs
@@ -6,13 +6,10 @@ module Main where
 import Lib (docx2html)
 
 import System.Environment (getArgs)
-import System.FilePath (replaceExtension, FilePath)
+import System.FilePath (FilePath)
 
-import Data.Semigroup ((<>))
-import Data.Text (Text)
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.Text.IO as T
-import qualified Data.Text as T
 
 main :: IO ()
 main = getArgs >>= \case
@@ -20,25 +17,4 @@ main = getArgs >>= \case
   _      -> error "Usage: pandoc_test <filename.docx>"
 
 convert :: FilePath -> IO ()
-convert f = BS.readFile f
-            >>= T.putStrLn
-            -- >>= T.writeFile (newFilename f)
-                -- . replaceDeletion
-                -- . replaceInsertion
-                . docx2html
-
-replaceTextWith :: Text -> Text -> Text -> Text
-replaceTextWith from to txt = go $ T.breakOn from txt
-   where
-    go (p, []) = p
-    go (p, s)  = p <> replaceTextWith from to (replaceText s)
-    replaceText t = to <> T.dropWhile ('>' /=) (T.drop (T.length from) t)
-
-replaceDeletion :: Text -> Text
-replaceDeletion = replaceTextWith "<span class=\"deletion\"" "<span text-decoration=\"line-through\""
-
-replaceInsertion :: Text -> Text
-replaceInsertion = replaceTextWith "<span class=\"insertion\"" "<span text-decoration=\"underline\""
-
-newFilename :: FilePath -> FilePath
-newFilename = flip replaceExtension "html"
+convert f = BS.readFile f >>= T.putStrLn . docx2html
